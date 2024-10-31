0e7dd8f2d553ee9bb758786626cf167ea36e4a39
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-10-31 14:48:53 +0200

    adding documentation

--- source/Game.h
+++ source/Game.h
@@ -5,13 +5,36 @@
 #ifndef GAME_H
 #define GAME_H
 
+////////// INCLUDE SECTION //////////
 #include <SFML/Graphics.hpp>
 
 #include "Actions.h"
 #include "EntityManager.h"
 
+////////// DESCRIPTION //////////
+
+/// todo - separate in 2 classes
+/// this class is a temporary class that functions
+/// like 2 different ones: GameEngine and Scene
+/// lots of the functionality were based on entity components that were public
+/// so this class just iterates through entities and call specific functions
+///
+/// this class manage the system events:
+/// - initialization
+/// --1 loading config files
+/// --1 --1 initializing window
+/// --1 --2 initializing font
+/// --2 setting up the action map (this will be moved on the scene class)
+/// - hande user input (raw)
+/// - movement & actions
+/// - rendering
+/// - collision / intersecting (soon implemented)
+/// - lifespan (not implemented)
+
+////////// SHORTCUTS //////////
 typedef std::map<int, std::string> ActionMap;
 
+////////// DEFINITION OF CLASS //////////
 class Game {
 
     bool m_running = true;
@@ -21,9 +44,9 @@ class Game {
     sf::RenderWindow m_window;
     sf::Font m_font;
     sf::Text m_text;
-    std::shared_ptr<Player> m_player;
+    std::shared_ptr<Player> m_player; // this might be temporary (can be implemented on scene
     EntityManager m_entityManager;
-    ActionMap m_actions;
+    ActionMap m_actions; // this will be implemented o  scene
 
     void init(const std::string& path);
 
