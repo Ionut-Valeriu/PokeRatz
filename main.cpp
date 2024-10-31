f7b9be3c5e689fc0539a553481434fbd8fb5655b
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-10-31 11:25:48 +0200

    removing unused libraries and comments

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-#include <iostream>
-#include <array>
-#include <chrono>
-#include <thread>
-
 #include <SFML/Graphics.hpp>
-
-#include <Helper.h>
 
 //////////////////////////////////////////////////////////////////////
 /// NOTE: this include is needed for environment-specific fixes     //
@@ -16,30 +9,13 @@
 
 #include "source/Game.h"
 
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
-public:
-    explicit SomeClass(int) {}
-};
-
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
-
-
 int main() {
     ////////////////////////////////////////////////////////////////////////
     /// NOTE: this function call is needed for environment-specific fixes //
     init_threads();                                                       //
     ////////////////////////////////////////////////////////////////////////
 
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;
-
-    Game g("config/config.txt");
+    Game g("resources/config/config.txt");
     g.run();
 
     return 0;
