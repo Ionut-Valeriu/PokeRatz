c73437933852cbb44554f6cc54a851c8288d89a2
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-10-31 19:44:33 +0200

    description added

--- source/Actions.h
+++ source/Actions.h
@@ -4,9 +4,16 @@
 
 #ifndef ACTIONS_H
 #define ACTIONS_H
+
+////////// INCLUDE SECTION //////////
 #include <string>
 
+////////// DESCRIPTION //////////
 
+///this class is used to define the action that can
+///take part of a scene, they are defined in init
+
+////////// DEFINITION OF CLASS //////////
 class Actions {
     std::string m_name = "NONE";
     std::string m_type = "NONE";
@@ -19,7 +26,5 @@ public:
     const std::string& name() const { return m_name; }
     const std::string& type() const { return m_type; }
 };
-
-
 
 #endif //ACTIONS_H
