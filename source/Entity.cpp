9fc8493310de9655b71f6cf24d97227854539557
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-11-01 14:17:03 +0200

    calling init in constructor

--- source/Entity.cpp
+++ source/Entity.cpp
@@ -3,17 +3,17 @@
 //
 
 #include "Entity.h"
-#include <iostream>
 
-Entity::Entity(size_t id, const std::string &tag) : m_id(id), m_tag(tag) {
-    tempShape = std::make_shared<sf::RectangleShape>(sf::Vector2f{100, 100});
+Entity::Entity(size_t id, std::string tag) : m_id(id), m_tag(std::move(tag)) {
+    // tempShape = std::make_shared<sf::RectangleShape>(sf::Vector2f{100, 100});
+    init();
 }
 
 void Entity::init() {
     tempShape = std::make_shared<sf::RectangleShape>(sf::Vector2f(100, 100));
 }
 
-void Entity::draw(sf::RenderWindow &window) {
+void Entity::draw(sf::RenderWindow &window) const {
     tempShape->setPosition(m_position);
     window.draw(*tempShape);
 }
