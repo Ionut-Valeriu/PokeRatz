3fcbed3ce552c36c4b3d4acb176e08230f2b91a3
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-11-01 14:16:08 +0200

    -add setVelocity using Enum -moving the player movement logic to Game

--- source/Player.cpp
+++ source/Player.cpp
@@ -3,35 +3,11 @@
 //
 
 #include "Player.h"
+#include "Enums.h"
 
 #include <iostream>
 
-void Player::setAction(Actions &action) {
-    std::cout << action.name() << " " << action.type() << std::endl;
-    if (action.type() == "START") {
-        if (action.name() == "UP") {
-            m_velocity.y = -m_speed;
-        } else if (action.name() == "DOWN") {
-            m_velocity.y = m_speed;
-        } else if (action.name() == "LEFT") {
-            m_velocity.x = -m_speed;
-        } else if (action.name() == "RIGHT") {
-            m_velocity.x = m_speed;
-        } else {
-            std::cerr << "[Player::setAction - START] Invalid action name: " << action.name() << std::endl;
-        }
-    } else if (action.type() == "END") {
-             if (action.name() == "UP"   ) { if (m_velocity.y < 0) { m_velocity.y = 0; } }
-        else if (action.name() == "DOWN" ) { if (m_velocity.y > 0) { m_velocity.y = 0; } }
-        else if (action.name() == "LEFT" ) { if (m_velocity.x < 0) { m_velocity.x = 0; } }
-        else if (action.name() == "RIGHT") { if (m_velocity.x > 0) { m_velocity.x = 0; } }
-        else {
-            std::cerr << "[Player::setAction - END] Invalid action name: " << action.name() << std::endl;
-        }
-    } else {
-        std::cerr << "[Player::setAction] Invalid action type: " << action.type() << std::endl;
-    }
-
-    std::cout << m_velocity.x << " " << m_velocity.y << std::endl;
-
+void Player::setVelocity(Move x, Move y) {
+    m_velocity.x = x * m_speed;
+    m_velocity.y = y * m_speed;
 }
