9df23b29c13a17eb462036b3b3c6eea90222955c
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-11-11 14:20:37 +0200

    + animation class

--- source/Animation.cpp
+++ source/Animation.cpp
@@ -3,3 +3,47 @@
 //
 
 #include "Animation.h"
+
+#include <cmath>
+#include <utility>
+
+Animation::Animation() = default;
+
+Animation::Animation(const std::string &name, const sf::Texture &t)
+    : Animation(name, t, 1, 0) {}
+
+Animation::Animation (std::string name, const sf::Texture & t, size_t frameCount, size_t speed)
+            : m_sprite(t)
+            , m_name(std::move(name))
+            , m_size (sf::Vector2f((float)t.getSize().x, (float)t.getSize().y))
+            , m_frameCount(frameCount)
+            , m_currentFrame(0)
+            , m_speed(speed)
+{
+    m_sprite.setOrigin(m_size.x / 2.0f, m_size.y / 2.0f);
+    m_sprite.setTextureRect(sf::IntRect(std::floor(m_currentFrame) * m_size.x, 0, std::floor(m_size.x), std::floor(m_size.y)));
+}
+
+void Animation::update() {
+    // todo
+
+    m_currentFrame++;
+
+    // todo calculate the frame of animation
+    // todo set the texture rect
+}
+
+// bool Animation::hasEnded() const {
+//     // todo - detect last frame
+//     return false;
+// }
+
+const std::string & Animation::getName() const { return m_name; }
+
+const sf::Vector2f & Animation::getSize() const { return m_size; }
+
+sf::Sprite & Animation::getSprite() { return m_sprite; }
+
+std::ostream & operator<<(std::ostream &os, const Animation &obj) {
+    return os << "m_name: " << obj.m_name;
+}
--- source/Animation.h
+++ source/Animation.h
@@ -6,11 +6,42 @@
 #define ANIMATION_H
 
 ////////// INCLUDE SECTION //////////
-
+#include <SFML/Graphics.hpp>
 ////////// DESCRIPTION //////////
 
 ////////// DEFINITION OF CLASS //////////
 class Animation {
+    // 288
+    sf::Sprite m_sprite;
+
+    // 32
+    std::string m_name = "none";
+
+    // 8
+    sf::Vector2f m_size = { 1, 1 };
+    size_t m_frameCount = 1;
+    size_t m_currentFrame = 0;
+    size_t m_speed = 0;
+
+public:
+    // constructors
+    Animation();
+    Animation (const std::string &name, const sf::Texture &t);
+    Animation (std::string name, const sf::Texture &t, size_t frameCount, size_t speed);
+
+    // methods
+    void update();
+    // bool hasEnded() const;
+
+    // getters
+    const std::string &getName() const;
+    const sf::Vector2f &getSize() const;
+    sf::Sprite &getSprite();
+
+    friend std::ostream & operator<<(std::ostream &os, const Animation &obj);
 };
+
+#include <string>
+
 
 #endif //ANIMATION_H
