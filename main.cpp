3897c963fdd71fa6b9ef4a7aa13390851f5a0f4a
ionut <valeriu-ionut.tablet@s.unibuc.ro>
2024-11-17 16:29:11 +0200

    + temporary background music

--- main.cpp
+++ main.cpp
@@ -4,16 +4,24 @@
 /// NOTE: this include is needed for environment-specific fixes     //
 /// You can remove this include and the call from main              //
 /// if you have tested on all environments, and it works without it //
-#include "env_fixes.h"                                              //
+// #include "env_fixes.h"                                              //
 //////////////////////////////////////////////////////////////////////
 
 #include "source/Game.h"
+#include <SFML/Audio.hpp>
 
 int main() {
     ////////////////////////////////////////////////////////////////////////
     /// NOTE: this function call is needed for environment-specific fixes //
-    init_threads();                                                       //
+    // init_threads();                                                       //
     ////////////////////////////////////////////////////////////////////////
+
+    // todo - move this as an asset
+    sf::SoundBuffer buffer;
+    buffer.loadFromFile("resources/sounds/BumBumBum");
+    sf::Sound sound(buffer);
+    sound.play();
+    ///
 
     Game g("resources/config/config.txt");
     g.run();
